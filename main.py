a34bbc98762b892de62596525ed6a8774786f273
pmankyu <pmankyu@gmail.com>
2021-08-30 14:36:10 +0900

    frame 추출 완료

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import cv2
 
 capture=cv2.VideoCapture("/home/pmk/Downloads/WIN_20210826_13_47_05_Pro.mp4")
 
-while cv2.waitKey(33)<0:
-  if capture.get(cv2.CAP_PROP_POS_FRAMES)==capture.get(cv2.CAP_PROP_FRAME_COUNT):
-    capture.set(cv2.CAP_PROP_POS_FRAMES,0)
+cnt=0
 
+while(capture.isOpened()):
   ret,frame=capture.read()
-  cv2.imshow("VideoFrame",frame)
+
+  if(int(capture.get(1))%10==0):
+    print('Saved frame number : ' + str(int(capture.get(1))))
+    cv2.imwrite("/home/pmk/workspace/frame_drop_tool/images/%05d.png" % cnt, frame)
+
+    cnt+=1
 
 capture.release()
 cv2.destroyAllWindows()
